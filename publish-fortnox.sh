87856b693c374cd1d0e564497443db14ee7ad6f3
Staffan Olsson <staffan@repos.se>
2024-12-06 14:34:51 +0100

    mavenrepo branch documents how to publish quarkus-fortnox

--- publish-fortnox.sh
+++ publish-fortnox.sh
@@ -3,13 +3,13 @@
 set -eo pipefail
 
 MVNREPOPROJECT=$PWD
-cd ../quarkus-gitea
+cd ../quarkus-fortnox
 git status && test -z "$(git status --porcelain)"
 GITREF=$(git rev-parse HEAD);
 mvn clean deploy
 cd $MVNREPOPROJECT
-cp -av ../quarkus-gitea/snapshots/se/yolean/quarkus-gitea* snapshots/se/yolean
-git add -f snapshots/se/yolean/quarkus-gitea*
+cp -av ../quarkus-fortnox/snapshots/se/yolean/quarkus-fortnox* snapshots/se/yolean
+git add -f snapshots/se/yolean/quarkus-fortnox*
 git diff --cached --stat
 git commit -m "quarkus-fortnox $GITREF"
 echo "TODO Review and push manually."
